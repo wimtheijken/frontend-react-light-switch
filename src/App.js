21fea1a17dec1a30979f41cf91f00f67cc3a563e
wimtheijken <info@theijken.nl>
2023-05-14 09:54:04 +0200

    aanpassing-troggle-switch

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-    const [light, setLight] = useState(false);
+    const [light, toggleLight] = useState(false);
     // setLight(true)
     console.log(light)
   return (
     <main className={light ? "on" : "off"}>
       <section>
         <div className="dot"></div>
-        <button type="button" onClick={light ? ()=> setLight(false) : ()=> setLight(true)}>{light ? "Turn off" : "Turn on"}</button>
+        <button type="button" onClick={()=> toggleLight(!light)}>{light ? "Turn off" : "Turn on"}</button>
       </section>
     </main>
   );
